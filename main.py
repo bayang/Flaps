ff120e0c2850c898e1e1a8688e487e67fcc022c7
bayang <bayang@users.noreply.github.com>
2015-08-05 21:10:13 +0200

    file path no longer hardcoded

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        f = request.files['file']
+        myfilename = f.filename
+        myfileurl = url_for('static', filename=myfilename)
+        return render_template('index.html', myfilename=myfilename,
+                               myfileurl=myfileurl)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

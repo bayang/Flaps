29b14739a2494cfcd784c6188d8a6c6b0f179a56
bayang <bayang@users.noreply.github.com>
2015-08-07 22:36:38 +0200

    trace now automatically centered on map

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, url_for, request, redirect
+import xml.etree.ElementTree as ET
+import os
+
 app = Flask(__name__)
+
+
+def getcoords(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    trk = root.find("[{http://www.topografix.com/GPX/1/1}trk]")
+    trkpt = trk.find('.//{http://www.topografix.com/GPX/1/1}trkpt')
+    x = trkpt.attrib
+    lat, lon = x['lat'], x['lon']
+    coords = [float(lon), float(lat)]
+    return coords
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -8,8 +22,9 @@ def index():
         f = request.files['file']
         myfilename = f.filename
         myfileurl = url_for('static', filename=myfilename)
+        centercoords=getcoords(os.path.join('static', myfilename))
         return render_template('index.html', myfilename=myfilename,
-                               myfileurl=myfileurl)
+                               myfileurl=myfileurl, centercoords=centercoords)
     return render_template('index.html')
 
 if __name__ == '__main__':
